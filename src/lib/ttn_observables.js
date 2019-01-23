ccc69c903b0ab2bd2701612b645471cf844d60da
Gilbert <gilbertlambermont@gmail.com>
2019-01-23 19:49:49 +0100

    Made sensor data an array

--- src/lib/ttn_observables.js
+++ src/lib/ttn_observables.js
@@ -1,8 +1,9 @@
-const ttncreds = require("../models/ttn_user"),
-    ttn = require("ttn"),
-    data = require("../models/sensor"),
-    supp = require("./ttn_support"),
-    socket = require("./sockets");
+const ttncreds = require("../models/ttn_user");
+const ttn = require("ttn");
+const data = require("../models/sensor");
+const supp = require("./ttn_support");
+const socket = require("./sockets");
+const obj = require("../lib/object");
 
 function startAll() {
     ttncreds.find({}).then((users) => {
@@ -18,14 +19,28 @@ function startOne(client, password) {
         c.on("uplink", (devId, payload) => {
             if (payload.dev_id == null) return;
             supp.translateTtnPayload(payload.payload_raw).forEach(item => {
-                data({
+                const dataset = {
                     sensor_name: payload.dev_id,
                     sensor_id: item[0],
-                    sensor_data: item[1],
-                    sensor_time: payload.metadata.time
+                    sensor_data: {
+                        time: payload.metadata.time,
+                        data: item[1]
+                    }
+                };
+
+                data.find({
+                    sensor_name: payload.dev_id,
+                }, (err, sensorData) => {
+                    if (obj.isEmpty(sensorData)) {
+                        if (error) return res.status(404).json({ "message": error });
+
+                        data(dataset).save();
+                    } else if (sensorData.sensor_uplink && Array.isArray(sensorData.sensor_uplink)) {
+                        sensorData.sensor_uplink.push(dataset.sensor_uplink);
+                    }
                 }).save();
-                socket.emit(payload.dev_id, { sensor_id: item[0], 
-                    sensor_data: item[1] });
+
+                socket.emit(payload.dev_id, { sensor_id: item[0], sensor_data: item[1] });
             });
         });
     }).catch(error => {
